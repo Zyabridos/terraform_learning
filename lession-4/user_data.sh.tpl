fc3f99d25e45f2d227ed036e33954c1b1d1e3b98
Nina Zyabrina <zyabrina95@gmail.com>
2025-05-21 21:06:11 +0200

    add lession about creation of dynamic security grioup

--- lession-4/user_data.sh.tpl
+++ lession-4/user_data.sh.tpl
@@ -6,13 +6,15 @@ myip=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
 
 cat <<EOF > /var/www/html/index.html
 <html>
-<h2>Build by Power of Terraform <font color="red> v.0.12</font>"</h2><br>
+<h2>Build by Power of Terraform <font color="red">v.0.12</font></h2><br>
 Owner ${first_name} ${last_name} <br>
 
 %{ for name in names ~}
 Hello ${name} from ${first_name}<br>
-%{ endof ~}
+%{ endfor ~}
+
 </html>
 EOF
+
 sudo service httpd start
 chkconfig httpd on
---
+++ lession-5/DynamicSecurityGroup.tf
@@ -0,0 +1,43 @@
+# --------------------------------
+# My Terraform 
+#
+# Build WebServer during Bootstrap
+# --------------------------------
+
+provider "aws" {
+  region = "eu-north-1"
+}
+
+resource "aws_security_group" "my_webServer" {
+  name = "Dynamic Security Group"
+
+  dynamic "ingress" {
+    for_each = ["80", "9092", "443", "8080"]
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"] 
+    }
+  }
+
+  ingress {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["10.10.0.0/16"] 
+  }
+  
+  # тут у нас будет уходить любой трафик
+  egress {
+      from_port   = 0 # любой порт
+      to_port     = 0 # любой порт
+      protocol    = "-1" # любой протокол
+      cidr_blocks = ["0.0.0.0/0"] # любой порт
+  }
+
+  tags = {
+    Name = "Web Server Security Group"
+    Owner = "Nina Zyabrina"
+  }
+}
