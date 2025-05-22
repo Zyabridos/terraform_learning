88d206595fd0bb3f6cfc2728ab603fbd84b7ed9e
Nina Zyabrina <zyabrina95@gmail.com>
2025-05-22 18:28:23 +0200

    add outputs to lession-6

--- lession-6/WebServer.tf
+++ lession-6/WebServer.tf
@@ -16,11 +16,6 @@ resource "aws_instance" "my_webServer" {
   ami                    = "ami-00f34bf9aeacdf007"
   instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.my_webServer.id]
-  user_data              = templatefile("user_data.sh.tpl", {
-    first_name = "Nina",
-    last_name  = "Ziabrina",
-    names      = ["Vasya", "Kolya", "John", "Donald", "Masha"] 
-  })
 
   tags = {
     Name = "Web Server Build by Terraform" # по факту ни на что не влияет, но куда логичнее и красивее задавать имена сервакам
@@ -28,8 +23,8 @@ resource "aws_instance" "my_webServer" {
   }
 
   lifecycle {
-    prevent_destroy = true
-    ignore_changes = ["ami", "user_data"]
+    prevent_destroy       = true
+    ignore_changes        = ["ami", "user_data"]
     create_before_destroy = true
   }
 }
@@ -59,4 +54,11 @@ resource "aws_security_group" "my_webServer" {
     Name = "Web Server Security Group"
     Owner = "Nina Zyabrina"
   }
+}
+
+output "web_server_instance_id" {
+  value = aws_instance.my_webServer.id
+}
+output "web_server_public_id_adress" {
+  value = aws_eip.my_static_ip.public_ip
 }
