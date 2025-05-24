5c63da810087d048babd7d3e72bd7ea8090336b0
Nina Zyabrina <zyabrina95@gmail.com>
2025-05-24 14:41:27 +0200

    add a lession about Autosearch for AMI

---
+++ lession-9/main.tf
@@ -0,0 +1,86 @@
+# -----------------------------------
+# Find latest AMI id of:
+#   - Ubuntu
+#   - Amazon Linux 2
+#   - Windows Server 2025 Base
+#
+# Made by Nina Ziabrina
+# -----------------------------------
+
+provider "aws" {
+  region = "eu-north-1"
+}
+
+data "aws_ami" "latest_Ubuntu" {
+  owners      = ["099720109477"]
+  most_recent = true
+  filter {
+    name   = "name"    # по какому ключу фильтровать
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]      # значение ключа
+  }
+  
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+data "aws_ami" "latest_amazon_linux" {
+  owners      = ["137112412989"]
+  most_recent = true
+  filter {
+    name   = "name"                                                       # по какому ключу фильтровать
+    values = ["amzn2-ami-kernel-5.10-hvm-2.0.20250512.0-x86_64-gp2"]      # значение ключа
+  }
+}
+
+data "aws_ami" "latest_Windows" {
+  owners      = ["801119661308"]
+  most_recent = true
+  filter {
+    name   = "name"                                                               # по какому ключу фильтровать
+    values = ["Windows_Server-2025-English-Full-Base-*"]      # значение ключа
+  }
+  
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+resource "aws_instance" "my_webServer_ubuntu" {
+  ami           = data.aws_ami.latest_Ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Ubuntu_Server"
+  }
+}
+
+resource "aws_instance" "my_webServer_Windows" {
+  ami           = data.aws_ami.latest_Windows.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Windows_Server"
+  }
+}
+
+resource "aws_instance" "my_webServer_Amazon" {
+  ami           = data.aws_ami.latest_amazon_linux.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "Amazon_Server"
+  }
+}
---
+++ lession-9/outputs.tf
@@ -0,0 +1,23 @@
+output "latest_amazon_linux_ami_id" {
+  value = data.aws_ami.latest_amazon_linux.id
+}
+
+output "latest_amazon_linux_ami_name" {
+  value = data.aws_ami.latest_amazon_linux.name
+}
+
+output "latest_Ubuntu_ami_id" {
+  value = data.aws_ami.latest_Ubuntu.id
+}
+
+output "latest_Ubuntu_ami_name" {
+  value = data.aws_ami.latest_Ubuntu.name
+}
+
+output "latest_Windows_id" {
+  value = data.aws_ami.latest_Windows
+}
+
+output "latest_Windows_ami_name" {
+  value = data.aws_ami.latest_Windows
+}
