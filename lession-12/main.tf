6ddcbdd9b28f3b8e7eb9e4c8d797313499f8db6d
Nina Zyabrina <zyabrina95@gmail.com>
2025-05-25 17:01:07 +0200

    add a lession about variables

---
+++ lession-12/main.tf
@@ -0,0 +1,73 @@
+# -----------------------------------
+# My Terraform
+#
+# Variables
+#
+#
+# -----------------------------------
+
+provider "aws" {
+  region = var.region
+}
+
+data "aws_ami" "latest_amazon_linux" {
+  owners      = ["137112412989"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm-2.0.20250512.0-x86_64-gp2"]
+  }
+}
+
+resource "aws_eip" "my_static_ip" {
+  instance = aws_instance.my_server.id
+
+  tags = merge(var.common_tags, 
+    {
+      Name = "Server IP"
+    })
+}
+
+resource "aws_instance" "my_server" {
+  ami                    = data.aws_ami.latest_amazon_linux.id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [aws_security_group.my_server.id]
+  monitoring             = var.detailed_monitoring # default false. Отправь результаты мониторинга сервера каждую минуту (мне точно не нужно, ибо будет дороже содержание серверов, но пусть для булевсуого примера будет)
+
+  tags = merge(var.common_tags, 
+    { 
+      Name = "Server Build By Terraform" 
+    })
+}
+
+resource "aws_security_group" "my_server" {
+  name = "My Security Group"
+
+  dynamic "ingress" {
+    for_each = var.allowed_ports
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"] 
+    }
+  }
+
+  ingress {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["10.10.0.0/16"] 
+  }
+  
+  egress {
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(var.common_tags, {
+    Name = "Server Security Group"
+  })
+}
---
+++ lession-12/variables.tf
@@ -0,0 +1,32 @@
+variable "region" {
+  description = "Please enter AWS region to deploy Server"
+  default     = "eu-north-1"
+}
+
+variable "instance_type" {
+  description = "Enter instance type"
+  default     = "t3.micro"
+}
+
+variable "allowed_ports" {
+  description = "List of ports to open for Server"
+  type        = list(number)
+  default     = [80, 443, 22, 8080]
+}
+
+variable "detailed_monitoring" {
+  description = "Enable detailed monitoring"
+  type        = bool
+  default     = true
+}
+
+variable "common_tags" {
+  description = "Common tags to apply to all resources"
+  type        = map
+  default     = {
+    Owner = "Zyabridos"
+    Project = "Phoenix"
+    CostSenter = "12345"
+    Enviorment = "development"
+  }
+}
